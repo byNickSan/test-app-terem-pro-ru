c1dfa35a7ee6296e241fe1ed72314fa14ec96f4d
byNickSan <nicksan92@gmail.com>
2021-02-20 14:57:40 +0300

    Create .description.php

---
+++ local/components/bitrix/terem-csv-parser/.description.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * .description.php
+ *
+ * @author     byNickSan
+ * @copyright  2021 Nikolay
+ * @license    https://mit-license.org  MIT License
+ */
+if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true)
+{
+	die();
+}
+
+$arComponentDescription = array(
+	"NAME" => "View ibblock Elements",
+	"DESCRIPTION" => "Test for Terem Company",
+	"ICON" => "/images/icon.gif",
+);
